5e77968ab64d38e9e4c577f1ba8cf89cb43563f3
GaneaFunpay <ganea.funpay@gmail.com>
2025-10-22 15:50:01 +0300

    Included mptt library to INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djoser',
+    'mptt',
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
